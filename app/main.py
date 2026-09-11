b5183d39c49bf7d73206dc61c3f142e084b8c877
Lucas Amaral <lucas.trevvos@gmail.com>
2026-09-11 20:40:17 -0300

    refactor: clean main imports

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from pathlib import Path
-from starlette.middleware.sessions import SessionMiddleware
-from app.boards.routes import router as boards_router
-
-from app.config import get_session_secret_key, is_production
-
-from app.boards.routes import router as boards_router
 
 from fastapi import FastAPI
-from app.auth.routes import router as auth_router
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+
+from app.auth.routes import router as auth_router
+from app.boards.routes import router as boards_router
+from app.config import get_session_secret_key, is_production
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
